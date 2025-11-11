380529e4573f317017115f606b5ce5fe446adec4
Rodrigo Oliveira <rodrigoalmeidadeoliveira@gmail.com>
2025-11-11 16:14:32 -0300

    perfil sensorial update

---
+++ migrations/versions/6bfe157efc7f_merge_perfil_sensorial_heads.py
@@ -0,0 +1,24 @@
+"""merge perfil sensorial heads
+
+Revision ID: 6bfe157efc7f
+Revises: 0d6e3b4fd6f0, 178d39359bbd
+Create Date: 2025-11-11 16:11:51.621610
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6bfe157efc7f'
+down_revision = ('0d6e3b4fd6f0', '178d39359bbd')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
